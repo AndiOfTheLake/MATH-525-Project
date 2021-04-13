8e2fb11053d86d20285db4f9ad30b0e7cd493e0b
AndiOfTheLake <46411699+AndiOfTheLake@users.noreply.github.com>
2021-04-13 19:21:47 -0400

    go from SSB to Delta

--- projT2.R
+++ projT2.R
@@ -16,18 +16,18 @@ N_h_cont<-c(446, 17175, 7011, 12550, 5472)
 
 cont<-c("Africa", "Americas", "Asia", "Europe", "Oceania")
 
-ybar_hu_hat<-c()
+ybar_hu_cont_hat<-c(); ssqr_cont<-c()
 for (h in 1:length(cont)){
 dt.h <- dt %>% filter(continent == cont[h]) %>% select(Rating) %>% pull()
-ybar_hu_hat[h]<-mean(dt.h)
+ybar_hu_cont_hat[h]<-mean(dt.h)
+ssqr_cont[h]<-as.numeric(var(dt.h))
 }
-
-ybar_hu_hat
 
 (ybar_hat<-mean(dt$Rating))
 
-(SSB_cont_hat<-((ybar_hu_hat-ybar_hat)^2*N_h_cont)%>% sum)
+(SSB_cont_hat<-((ybar_hu_cont_hat-ybar_hat)^2*N_h_cont)%>% sum) # SSB based on "continent"
 
+(Delta_cont_hat<-SSB_cont_hat-sum(ssqr_cont)) # Delta hat based on "continent"
 
 ## ---- sub-task1-branch ----
 # branch variable
@@ -36,26 +36,18 @@ N_h_brch<-c(19406, 9619, 13629)
 
 brch<-c("Disneyland_California",  "Disneyland_HongKong",  "Disneyland_Paris")
 
-ybar_hu_hat<-c()
+ybar_hu_brch_hat<-c(); ssqr_brch<-c()
 for (h in 1:length(brch)){
   dt.h <- dt %>% filter(Branch == brch[h]) %>% select(Rating) %>% pull()
-  ybar_hu_hat[h]<-mean(dt.h)
+  ybar_hu_brch_hat[h]<-mean(dt.h)
+  ssqr_brch[h]<-as.numeric(var(dt.h))
 }
 
-ybar_hu_hat 
-
-(SSB_brch_hat<-((ybar_hu_hat-ybar_hat)^2*N_h_brch)%>% sum)
+(SSB_brch_hat<-((ybar_hu_brch_hat-ybar_hat)^2*N_h_brch)%>% sum) # SSB based on "Branch"
+(Delta_brch_hat<-SSB_brch_hat-sum(ssqr_brch)) # Delta hat based on "Branch"
 
 ## ---- sub-task2 ----
-ssqr_h <- c()
-for (h in 1:length(brch)){
-  dt.h <- dt %>% filter(Branch == brch[h]) %>% select(Rating) %>% pull()
-  ssqr_h[h]<-as.numeric(var(dt.h))
-}
-
-ssqr_h # estimated within-stratum variances
-
 n<-6000 # total sample size
 
 # optimal sample sizes from each stratum
-(n_h<-(n*N_h_brch*sqrt(ssqr_h)/sum(N_h_brch*sqrt(ssqr_h))) %>% round)
+(n_h<-(n*N_h_brch*sqrt(ssqr_brch)/sum(N_h_brch*sqrt(ssqr_brch))) %>% round)
