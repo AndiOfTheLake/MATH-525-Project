bdcb7f7347f0e4ff9c219e88499bde5386f4770b
AndiOfTheLake <46411699+AndiOfTheLake@users.noreply.github.com>
2021-04-20 20:00:32 -0400

    redo subtask2

--- projT3.R
+++ projT3.R
@@ -20,9 +20,11 @@ colnames(dtB_bybrch)[3]<- "Average_of_Ratings"
 
 
 ## ---- sub task 1 ----
-dtB_overall
-dtB_overall$t_i<-(dtB_overall$Average_of_Ratings)*(dtB_overall$m_i) # cluster total
+# we are using the number of observations as given in the tables
+# i.e. N_pop=19406+9619+13629=42654
+(N_pop<-19406+9619+13629)
 
+dtB_overall$t_i<-(dtB_overall$Average_of_Ratings)*(dtB_overall$m_i) # cluster total
 
 (pi<-diag(p)) # first order inclusion probabilities
 
@@ -31,7 +33,7 @@ sum(dtB_overall$t_i/pi)
 
 # Hence the population mean is estimated to be 
 # (we are using the number of observations as given in the tables)
-(y_bar_HT<-sum(dtB_overall$t_i/pi)/(19406+9619+13629))
+(y_bar_HT<-sum(dtB_overall$t_i/pi)/(N_pop))
 
 # Now we find the variance of the estimator for the total
 tmp<-matrix(NA, 15, 15)
@@ -46,17 +48,73 @@ for (i in (1:15)){
   as.numeric)
 
 # hence we obtain estimated SE for population mean
-(SE_y_bar_HT<-sqrt(var_y_HT)/(19406+9619+13629))
+(SE_y_bar_HT<-sqrt(var_y_HT)/N_pop)
 
 # hence the 95% CI for the population mean is
 y_bar_HT + qnorm(c(0.025, 0.975))*SE_y_bar_HT
 
 
 
+
+
 ## ---- sub task 2 ----
+N_h_brch<-c(19406, 9619, 13629) # population branch sizes
+
+
+brch<-c("Disneyland_California",  
+        "Disneyland_HongKong",  
+        "Disneyland_Paris") # Branch names
+
+# add a column of cluster totals to data frame
+dtB_bybrch$t_i<-(dtB_bybrch$Average_of_Ratings)*(dtB_bybrch$m_i)
+
+ssqr<-c(); t_hat<-c(); y_bar_hat<-c(); SE_y_bar_hat<-c()
+for (h in 1:length(brch)){
+  
+  dtB.h <- dtB_bybrch %>% filter(Branch == brch[h]) # subsetting by Branch
+  
+  ssqr[h]<-t(dtB.h$t_i) %*% tmp %*% (dtB.h$t_i) # estimated variance of branch-specific total
+  
+  t_hat[h]<-sum(dtB.h$t_i/pi) # estimated branch total
+  
+  y_bar_hat[h]<-t_hat[h]/N_h_brch[h] # estimated branch average
+  
+  SE_y_bar_hat[h]<-sqrt(ssqr[h])/N_h_brch[h] # estimated SE
+}
+
+CI<-cbind(brch, 
+          y_bar_hat %>% round(.,3),
+          (y_bar_hat + qnorm(0.025))%>% round(.,3),
+          (y_bar_hat + qnorm(0.975))%>% round(.,3)
+)  %>% 
+  data.frame() %>%
+  format(., justify="left") 
+
+colnames(CI)<-c("BRANCH",
+                "Estimated average",
+                "2.5%",
+                "97.5%")
+CI # 95% CI
+
+# The confidence intervals are very, very wide and they overlap.
+
+# domain estimation (Ratio estimator) (I cannot figure out what to do)
+dtB_bybrch$fpc<-15
+onestage_drv_design = svydesign(id=~Year_Month,fpc=~fpc,
+                                data=dtB_bybrch)
+onestage_drv_design
+
+svyby(~Average_of_Ratings, ~Branch, design = onestage_drv_design, svymean)
+
 
 
 ## ---- sub task 3 ----
+
+
+
+
+
+
 
 ## ---- sub task 1 script----
 t_i<-(dtB_overall$Average_of_Ratings)*(dtB_overall$m_i) # cluster total
@@ -105,11 +163,16 @@ dtB_bybrch$t_i<-(dtB_bybrch$Average_of_Ratings)*(dtB_bybrch$m_i)
 
 ssqr<-c(); t_hat<-c(); y_bar_hat<-c(); SE_y_bar_hat<-c()
 for (h in 1:length(brch)){
+  
   dtB.h <- dtB_bybrch %>% filter(Branch == brch[h]) # subsetting by Branch
-  ssqr[h]<-(dtB.h$t_i)%>% var() # sample variance of cluster totals by Branch
-  t_hat[h]<-103/15*sum(dtB.h$t_i) # estimated cluster totals
-  y_bar_hat[h]<-t_hat[h]/N_h_brch[h] # estimated cluster means
-  SE_y_bar_hat[h]<-((1-15/103)*ssqr[h]/15) %>% sqrt()*103/N_h_brch[h] # estimated SE
+  
+  ssqr[h]<-t(dtB.h$t_i)%*% tmp %*% (dtB.h$t_i) # estimated variance of branch totals by Branch
+  
+  t_hat[h]<-sum(dtB.h$t_i/pi) # estimated branch total
+  
+  y_bar_hat[h]<-t_hat[h]/N_h_brch[h] # estimated branch average
+  
+  SE_y_bar_hat[h]<-sqrt(ssqr[h])/N_h_brch[h] # estimated SE
 }
 
 CI<-cbind(brch, 
