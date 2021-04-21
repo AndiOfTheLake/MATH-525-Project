5b8f7f35b0a7f26056236e59c7fe44cf785e7bc9
AndiOfTheLake <46411699+AndiOfTheLake@users.noreply.github.com>
2021-04-21 18:02:48 -0400

    change N

--- projT1.R
+++ projT1.R
@@ -12,7 +12,7 @@ dt<-read.csv("Employee_A_data.csv")
 ## ---- sub task 1----
 # This is an SRS of size 6000, not a stratified sample
 # drv: Disney Review
-N<-40041
+#N<-40041
 N<-19406 + 9619 + 13629
 n<-6000
 drv_srs_design = svydesign(id=~1, data=dt, fpc=rep(N, n))
